a233ac78d9d3780555f15ce92b12843fb2dc4372
chayank <chayank.tm@gmail.com>
2018-09-21 11:35:13 +0530

    admin aspx fix and admin layout fixed. prajjwal needs to make changes to add user aspx

--- StockTradingPlatform/Admin/AddUser.aspx.designer.cs
+++ StockTradingPlatform/Admin/AddUser.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace StockTradingPlatform.Admin {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
         
         /// <summary>
+        /// HyperLink7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink7;
+        
+        /// <summary>
         /// HyperLink3 control.
         /// </summary>
         /// <remarks>
