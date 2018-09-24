8c9d3311b25e92b60e619d352c91a5525df3f1b3
chayank <chayank.tm@gmail.com>
2018-09-24 11:54:42 +0530

    about us added

--- StockTradingPlatform/Controllers/UserController.cs
+++ StockTradingPlatform/Controllers/UserController.cs
@@ -276,9 +276,14 @@ namespace StockTradingPlatform.Controllers
             return View(manager);
         }
 
-        /// <summary>
-        ///  Chayank's code ends
-        /// </summary>
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
 
-    }
+            /// <summary>
+            ///  Chayank's code ends
+            /// </summary>
+
+        }
 }
